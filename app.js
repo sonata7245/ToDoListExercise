d199eca50a7ec4be0f5a0e83e4ce8a28de714831
sonata7245 <38821732+sonata7245@users.noreply.github.com>
2022-09-12 10:07:24 -0700

    Create Todo program

---
+++ app.js
@@ -0,0 +1,52 @@
+/* get user input to decide what to do */
+let userAction = toLowerCase(prompt("What would you like to do?"));
+
+const toDoList = [];
+
+/* while until quit run program */
+
+while (userAction !== quit){
+    /* add new todo */
+    if (userAction === "new"){
+        let newToDo = prompt("Please enter the new ToDo");
+        let addedItem = toDoList.push(newToDo);
+      console.log(addedItem);
+    }
+
+    /* list todos */
+    else if (userAction === "list"){
+        console.log("*****************************");
+        for (let i = 0; i < toDoList.length; i++){
+            console.log(`${i} : ${toDoList[i]}`);
+        }
+        console.log("*****************************");
+
+    }
+    /* delete todo */
+    else if (userAction === "delete"){
+       let deleteIndex = parseInt(prompt("Please enter the index of the item you want to delete"));
+
+       while (!deleteIndex || deleteIndex >= toDoList.length) {
+        deleteIndex = parseInt(prompt(`Please enter a valid number. Between 0 & ${ToDoList.length - 1}`));
+       }
+
+       let deletedItem = toDoList.splice(deleteIndex, 1);
+       console.log(deletedItem);
+       
+
+    }
+    /*quit program*/
+    else if (userAction === "quit") {
+        console.log("Quitting");
+        break;
+    }
+
+    else {
+        alert("Please enter a valid action item. You can enter new, list, delete or quit");    
+    }
+
+    userAction = toLowerCase(prompt("What would you like to do?"));
+
+}
+
+console.log("Thank you for using the ToDo List");
